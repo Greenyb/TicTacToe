69a0f9524370f39672ac5bd23dfd0104ef94452b
Greenyb <GREENBRYAN694@GMAIL.COM>
2021-06-15 17:45:34 -0500

    board

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.util.StringJoiner;
+
 public class Main {
 
-    public static void main(String[] args){
-        System.out.println("hello");
+    public static void showBoard(String[][] gameBoard){
+        for(int i = 0; i < 3; i++){
+            System.out.println(String.join("|", gameBoard[i]));
+            if(i < 2){
+                System.out.println("-+-+-");
+            }
+        }
+    }
+    public static void main(String[] args) {
+
+        System.out.println("\n\n*****TIC-TAC-TOE*****");
+        String[][] gameBoard = {{" ", " ", " "},
+                                {" ", " ", " "}, {" ", " ", " "}};
+        showBoard(gameBoard);
+
+
     }
 }
