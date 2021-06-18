895506ead91c6422fc434561f4e1354afdef29b1
markus1922 <81990350+markus1922@users.noreply.github.com>
2021-06-18 08:49:53 -0500

    Up

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,22 @@
+import java.util.Random;
 import java.util.StringJoiner;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void showBoard(String[][] gameBoard){
+     static String[][] gameBoard = {{" ", " ", " "},
+            {" ", " ", " "}, {" ", " ", " "}};
+    public static void showBoard(String[][] gameBoard) {
 
-        for(int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++) {
             System.out.println(String.join("|", gameBoard[i]));
-            if(i < 2){
+            if (i < 2) {
                 System.out.println("-+-+-");
             }
         }
     }
-    public static void playGame(String[][]  gameBoard) {
+
+    public static void playGame(String[][] gameBoard) {
         Scanner userInput = new Scanner(System.in);
         System.out.println("Enter Column");
         String column = userInput.nextLine();
@@ -23,22 +27,18 @@ public class Main {
     }
 
     public static void update(int column, int row) {
-    gameBoard[column][row] = 'X';
-        Random rand = new Random();
-        while(gameboard[random.randint(0, 2)][random.randint(0,2 ) ] == "X" )  {
+        gameBoard[column][row] = "X";
+        Random random = new Random();
+        while (gameBoard[random.nextInt(3)][random.nextInt(3)] == "X") {
 
         }
-    public static void main(String[] args) {
-
-        System.out.println("\n\n*****TIC-TAC-TOE*****");
-        String[][] gameBoard = {{" ", " ", " "},
-                                {" ", " ", " "}, {" ", " ", " "}};
-        showBoard(gameBoard);
-        playGame(gameBoard);
-        System.out.println(gameBoard);
     }
-}
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
+            System.out.println("\n\n*****TIC-TAC-TOE*****");
 
+            showBoard(gameBoard);
+            playGame(gameBoard);
+            System.out.println(gameBoard);
+        }
     }
